62dd0a4c92c36c7a06c0f7c2acd23d4547f85ac9
KevinLai02 <jihn.smith629@gmail.com>
2024-09-11 20:25:36 +0800

    link alpaca api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,32 +1,12 @@
-// import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import axios from "axios";
 
-// export async function apiHandler<T = any>(
-//   params: AxiosRequestConfig,
-// ) {
-//   try {
-//     const apiEndpoint = process.env.API_URL
-//     const request = axios.create({
-//       baseURL: '',
-//       timeout: 60 * 5000,
-//     })
-
-//     const res = await request.request<T>({ ...params })
-
-//     return res
-//   } catch (error) {
-//     const resError = error as AxiosError
-//     console.log(resError);
-
-//     }
-//   }
-
-//   export const callResume = async() => {
-//     const request = axios({
-//       method: 'get', // or 'post', 'put', etc.
-//       url: 'https://966c-1-172-243-31.ngrok-free.app/resume',
-//       headers: {
-//           'Content-Type': 'application/json'
-//       },
-//     })
-//     return request
-//   }
+export const callResume = async () => {
+  const request = axios({
+    method: "get", // or 'post', 'put', etc.
+    url: "http://127.0.0.1:5000/resume",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return request;
+};
