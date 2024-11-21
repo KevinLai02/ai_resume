a3f8b4c30f1a90a0787ff0a9114ad730f6c13153
C110181147 <c110181147@nkust.edu.tw>
2024-11-21 15:44:54 +0800

    Modify signup and login page

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -4,6 +4,7 @@ import Header from "../Main/components/Header";
 import { useForm } from "react-hook-form";
 import Button from "@/containers/Main/components/Button";
 import { callLogin } from "@/api/api";
+import Link from "next/link";
 
 function Login() {
   const {
@@ -23,36 +24,42 @@ function Login() {
   return (
     <div className="flex flex-col flex-1 items-center h-screen">
       <Header />
-      <div className="flex flex-1 justify-center items-center">
-        <div className="flex flex-col bg-custom-blue-300 items-center p-5 rounded-xl w-[30%] min-w-[510px] font-bold drop-shadow-md">
-          <p className="text-white text-2xl">登入</p>
-          <div className="flex flex-col w-full px-10 pb-10 my-4 bg-white rounded-xl">
-            <div className="mt-10">
-              <p className="text-black text-sm">信箱</p>
-              <input
-                className="w-full border text-sm border-gray-300 rounded-xl p-2 focus:outline-none drop-shadow"
-                {...register("email", { required: "*此欄位必填" })}
-              />
-              <p className="h-5 text-sm text-custom-red-100">
-                {errors.email?.message}
-              </p>
-            </div>
-            <div className="mt-2">
-              <p className="text-black text-sm">密碼</p>
-              <input
-                className="w-full border border-gray-300 text-sm rounded-xl p-2 focus:outline-none drop-shadow"
-                {...register("password", { required: "*此欄位必填" })}
-              />
-              <p className="h-5 text-sm text-custom-red-100">
-                {errors.password?.message}
-              </p>
+      <div className="flex flex-col items-center p-5 rounded-xl w-[30%] min-w-[510px] font-bold drop-shadow-md">
+        <div className="flex flex-col w-full px-10 pb-10 my-4 bg-white rounded-xl">
+          <div className="mt-10">
+            <div className="text-3xl font-bold">Login</div>
+            <div className="mt-1">
+              {"You don't have account? "}
+              <Link href={"/signup"} className="underline">
+                Click here to signup
+              </Link>
             </div>
           </div>
+          <div className="mt-5">
+            <p className="text-black text-sm">信箱</p>
+            <input
+              className="w-full border text-sm border-gray-300 rounded-xl px-2 py-3 focus:outline-none"
+              {...register("email", { required: "*此欄位必填" })}
+            />
+            <p className="h-5 text-sm text-custom-red-100">
+              {errors.email?.message}
+            </p>
+          </div>
+          <div className="mt-2">
+            <p className="text-black text-sm">密碼</p>
+            <input
+              className="w-full border border-gray-300 text-sm rounded-xl px-2 py-3 focus:outline-none"
+              {...register("password", { required: "*此欄位必填" })}
+            />
+            <p className="h-5 text-sm text-custom-red-100">
+              {errors.password?.message}
+            </p>
+          </div>
           <Button
-            className="p-3 rounded-lg bg-white min-w-[350px]"
+            className="mt-5 p-3 rounded-full bg-purple-600	min-w-[350px]"
             onClick={handleSubmit(onSubmit)}
           >
-            <div className="text-lg font-bold">登入</div>
+            <div className="text-lg font-bold text-white">登入</div>
           </Button>
         </div>
       </div>
--- src/containers/SignUp/SignUp.tsx
+++ src/containers/SignUp/SignUp.tsx
@@ -4,6 +4,7 @@ import Header from "../Main/components/Header";
 import { useForm } from "react-hook-form";
 import Button from "@/containers/Main/components/Button";
 import { callSignUp } from "@/api/api";
+import Link from "next/link";
 
 function SignUp() {
   const {
@@ -28,46 +29,52 @@ function SignUp() {
   return (
     <div className="flex flex-col flex-1 items-center h-screen">
       <Header />
-      <div className="flex flex-1 justify-center items-center">
-        <div className="flex flex-col bg-custom-blue-300 items-center p-5 rounded-xl w-[30%] min-w-[510px] font-bold drop-shadow-md">
-          <p className="text-white text-2xl">SignUp</p>
-          <div className="flex flex-col w-full px-10 pb-10 my-4 bg-white rounded-xl">
-            <div className="mt-10">
-              <p className="text-black text-sm">名稱</p>
-              <input
-                className="w-full text-sm border border-gray-300 rounded-xl p-2 focus:outline-none drop-shadow"
-                {...register("name", { required: "*此欄位必填" })}
-              />
-              <p className="h-5 text-sm text-custom-red-100">
-                {errors.name?.message}
-              </p>
-            </div>
-            <div className="mt-2">
-              <p className="text-black text-sm">信箱</p>
-              <input
-                className="w-full border text-sm border-gray-300 rounded-xl p-2 focus:outline-none drop-shadow"
-                {...register("email", { required: "*此欄位必填" })}
-              />
-              <p className="h-5 text-sm text-custom-red-100">
-                {errors.email?.message}
-              </p>
-            </div>
-            <div className="mt-2">
-              <p className="text-black text-sm">密碼</p>
-              <input
-                className="w-full border border-gray-300 text-sm rounded-xl p-2 focus:outline-none drop-shadow"
-                {...register("password", { required: "*此欄位必填" })}
-              />
-              <p className="h-5 text-sm text-custom-red-100">
-                {errors.password?.message}
-              </p>
+      <div className="flex flex-col items-center p-5 rounded-xl w-[30%] min-w-[510px] font-bold drop-shadow-md">
+        <div className="flex flex-col w-full px-10 pb-10 my-4 bg-white rounded-xl">
+          <div className="mt-10">
+            <div className="text-3xl font-bold">Sign Up</div>
+            <div className="mt-1">
+              {"You have account? "}
+              <Link href={"/login"} className="underline">
+                Click here to login
+              </Link>
             </div>
           </div>
+          <div className="mt-5">
+            <p className="text-black text-sm">名稱</p>
+            <input
+              className="w-full text-sm border border-gray-300 rounded-xl px-2 py-3 focus:outline-none"
+              {...register("name", { required: "*此欄位必填" })}
+            />
+            <p className="h-5 text-sm text-custom-red-100">
+              {errors.name?.message}
+            </p>
+          </div>
+          <div className="mt-2">
+            <p className="text-black text-sm">信箱</p>
+            <input
+              className="w-full border text-sm border-gray-300 rounded-xl px-2 py-3 focus:outline-none"
+              {...register("email", { required: "*此欄位必填" })}
+            />
+            <p className="h-5 text-sm text-custom-red-100">
+              {errors.email?.message}
+            </p>
+          </div>
+          <div className="mt-2">
+            <p className="text-black text-sm">密碼</p>
+            <input
+              className="w-full border border-gray-300 text-sm rounded-xl px-2 py-3 focus:outline-none"
+              {...register("password", { required: "*此欄位必填" })}
+            />
+            <p className="h-5 text-sm text-custom-red-100">
+              {errors.password?.message}
+            </p>
+          </div>
           <Button
-            className="p-3 rounded-lg bg-white min-w-[350px]"
+            className="mt-5 p-3 rounded-full bg-purple-600	min-w-[350px]"
             onClick={handleSubmit(onSubmit)}
           >
-            <div className="text-lg font-bold">註冊</div>
+            <div className="text-lg font-bold text-white">註冊</div>
           </Button>
         </div>
       </div>
